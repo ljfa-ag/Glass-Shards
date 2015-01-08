9b47ab0b496faf2bd30a7b219b8bb1b18b14041a
ljfa-ag <ljfa-ag@web.de>
2015-01-08 13:51:24 +0100

    Add basic config

---
+++ src/main/java/ljfa/glassshards/Config.java
@@ -0,0 +1,38 @@
+package ljfa.glassshards;
+
+import java.io.File;
+
+import net.minecraftforge.common.config.Configuration;
+import cpw.mods.fml.client.event.ConfigChangedEvent;
+import cpw.mods.fml.common.FMLCommonHandler;
+import cpw.mods.fml.common.eventhandler.SubscribeEvent;
+
+public class Config {
+    public static Configuration conf;
+    
+    public static final String CATEGORY_GENERAL = "general";
+    
+    public static void loadConfig(File file) {
+        if(conf == null)
+            conf = new Configuration(file);
+        
+        conf.load();
+        loadValues();
+        
+        FMLCommonHandler.instance().bus().register(new ChangeHandler());
+    }
+    
+    public static void loadValues() {
+        if(conf.hasChanged())
+            conf.save();
+    }
+    
+    /** Reloads the config values upon change */
+    public static class ChangeHandler {
+        @SubscribeEvent
+        public void onConfigChanged(ConfigChangedEvent.OnConfigChangedEvent eventArgs) {
+            if(eventArgs.modID.equals(Reference.MODID))
+                loadValues();
+        }
+    }
+}
