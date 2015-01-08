56ea1acbd60fd20a2ca3de4e45310e821898dab2
ljfa-ag <ljfa-ag@web.de>
2015-01-08 11:15:05 +0100

    Basic mod things

---
+++ src/main/java/ljfa/glassshards/GlassShards.java
@@ -0,0 +1,31 @@
+package ljfa.glassshards;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.SidedProxy;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid = Reference.MODID, name = Reference.MODNAME, version = Reference.VERSION)
+public class GlassShards {
+    @Mod.Instance(Reference.MODID)
+    public static GlassShards instance;
+    
+    /*@SidedProxy(clientSide = Reference.CLIENT_PROXY_CLASS, serverSide = Reference.SERVER_PROXY_CLASS)
+    public static CommonProxy proxy;*/
+    
+    @Mod.EventHandler
+    public void preInit(FMLPreInitializationEvent event) {
+        
+    }
+    
+    @Mod.EventHandler
+    public void init(FMLInitializationEvent event) {
+        
+    }
+    
+    @Mod.EventHandler
+    public void postInit(FMLPostInitializationEvent event) {
+        
+    }
+}
