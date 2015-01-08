0b753be3ce32c6c871c41ce8d1a1c456d74a075b
ljfa-ag <ljfa-ag@web.de>
2015-01-08 13:48:45 +0100

    Add some comments

--- src/main/java/ljfa/glassshards/util/GlassHelper.java
+++ src/main/java/ljfa/glassshards/util/GlassHelper.java
@@ -8,6 +8,7 @@ import net.minecraft.block.BlockStainedGlassPane;
 import net.minecraft.block.material.Material;
 
 public class GlassHelper {
+    /** Represents if a block is a glass block or pane */
     public static enum GlassType {
         block(1.0f), pane(0.375f);
         
@@ -16,6 +17,9 @@ public class GlassHelper {
         private GlassType(float mult) { multiplier = mult; }
     }
     
+    /** Checks if the block is some kind of glass
+     * @return A GlassType representing the type, or null if it is not a glass block
+     */
     public static GlassType getType(Block block, int meta) {
         if(block instanceof BlockGlass || block instanceof BlockStainedGlass)
             return GlassType.block;
@@ -26,10 +30,12 @@ public class GlassHelper {
             return null;
     }
     
+    /** Checks if the glass block is stained */
     public static boolean isGlassStained(Block block, int meta) {
         return block instanceof BlockStainedGlass || block instanceof BlockStainedGlassPane;
     }
     
+    /** Returns the color for stained glass */
     public static int getGlassColor(Block block, int meta) {
         return meta;
     }
