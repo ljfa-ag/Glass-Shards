e5b349ddcbd52bfb58a1445a8bc96c61263d0e50
ljfa-ag <ljfa-ag@web.de>
2015-01-09 22:02:35 +0100

    add to GlassType

--- src/main/java/ljfa/glassshards/util/GlassType.java
+++ src/main/java/ljfa/glassshards/util/GlassType.java
@@ -1,5 +1,13 @@
 package ljfa.glassshards.util;
 
+import ljfa.glassshards.util.GlassHelper.GlassShape;
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockGlass;
+import net.minecraft.block.BlockPane;
+import net.minecraft.block.BlockStainedGlass;
+import net.minecraft.block.BlockStainedGlassPane;
+import net.minecraft.block.material.Material;
+
 /** Represents a certain type of glass for the purpose of shards */
 public class GlassType {
     /** Represents if a block is a glass block or pane */
@@ -23,5 +31,21 @@ public class GlassType {
     
     public GlassType(GlassShape shape) {
         this(shape, false, -1);
+    }
+    
+    /** Checks if the block is some kind of glass
+     * @return A GlassShape representing the type, or null if it is not glass
+     */
+    public static GlassType getType(Block block, int meta) {
+        if(block instanceof BlockGlass)
+            return new GlassType(GlassShape.block);
+        else if(block instanceof BlockStainedGlass)
+            return new GlassType(GlassShape.block, true, meta);
+        else if(block instanceof BlockPane && block.getMaterial() == Material.glass)
+            return new GlassType(GlassShape.pane);
+        else if(block instanceof BlockStainedGlassPane)
+            return new GlassType(GlassShape.pane, true, meta);
+        else
+            return null;
     }
 }
