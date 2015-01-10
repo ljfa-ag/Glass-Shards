4a0bf4815a265fb52866d2f904dab0bc6052c728
ljfa-ag <ljfa-ag@web.de>
2015-01-10 12:34:44 +0100

    Add comments

--- src/main/java/ljfa/glassshards/util/GlassType.java
+++ src/main/java/ljfa/glassshards/util/GlassType.java
@@ -8,27 +8,44 @@ import net.minecraft.block.BlockStainedGlass;
 import net.minecraft.block.BlockStainedGlassPane;
 import net.minecraft.block.material.Material;
 
-/** Represents a certain type of glass for the purpose of shards */
+/**
+ * @author ljfa-ag
+ * Represents the traits of a glass block. Or pane or anything made of glass really.
+ */
 public class GlassType {
+    /** The multiplier for glass blocks. */
     public static final float mult_block = 1.0f;
+    /** The multiplier for glass panes. */
     public static final float mult_pane = 0.375f;
     
+    /** How much of a full block's of glass it is worth. */
     public final float multiplier;
+    /** Whether the glass is stained. */
     public final boolean isStained;
+    /** The color of the glass. This is the same as vanilla stained glass metadata. */
     public final int color;
     
+    /**
+     * @param multiplier How much of a full block's of glass it is worth. Should be either mult_block or mult_pane.
+     * @param stained Whether the glass is stained.
+     * @param color The color of the glass, just like vanilla stained glass metadata.
+     */
     public GlassType(float multiplier, boolean stained, int color) {
         this.multiplier = multiplier;
         this.isStained = stained;
         this.color = color;
     }
     
+    /**
+     * Constructor for non-stained glass.
+     * @param multiplier How much of a full block's of glass it is worth. Should be either mult_block or mult_pane.
+     */
     public GlassType(float multiplier) {
         this(multiplier, false, -1);
     }
     
     /** Checks if the block is some kind of glass
-     * @return A GlassShape representing the type, or null if it is not glass
+     * @return A GlassType representing the type, or null if it is not glass
      */
     public static GlassType getType(Block block, int meta) {
         if(GlassShards.chiselHelper != null) {
