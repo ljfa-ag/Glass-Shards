9a2a7d4077bc08100a50d00fa0e87b78d855b024
ljfa-ag <ljfa-ag@web.de>
2015-01-11 18:14:27 +0100

    Add to the renderer
    More item rendering
    
    Refactoring
    
    Less code for the renderer
    
    Imports
    
    Add to the renderer

--- src/main/java/ljfa/glassshards/render/TransparentItemRenderer.java
+++ src/main/java/ljfa/glassshards/render/TransparentItemRenderer.java
@@ -1,44 +1,48 @@
 package ljfa.glassshards.render;
 
-import org.lwjgl.opengl.GL11;
-
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.ItemRenderer;
-import net.minecraft.client.renderer.RenderBlocks;
 import net.minecraft.client.renderer.Tessellator;
-import net.minecraft.entity.item.EntityItem;
+import net.minecraft.client.renderer.entity.RenderItem;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.IIcon;
 import net.minecraftforge.client.IItemRenderer;
 
-public class TransparentItemRenderer implements IItemRenderer {
+import org.lwjgl.opengl.GL11;
 
+public class TransparentItemRenderer implements IItemRenderer {
+    
     @Override
     public boolean handleRenderType(ItemStack item, ItemRenderType type) {
-        return type == ItemRenderType.ENTITY;
+        return Minecraft.getMinecraft().gameSettings.fancyGraphics &&
+               (type == ItemRenderType.ENTITY
+            || type == ItemRenderType.EQUIPPED
+            || type == ItemRenderType.EQUIPPED_FIRST_PERSON);
     }
 
     @Override
     public boolean shouldUseRenderHelper(ItemRenderType type, ItemStack item, ItemRendererHelper helper) {
         return helper == ItemRendererHelper.ENTITY_BOBBING
-                || helper == ItemRendererHelper.ENTITY_ROTATION;
+            || helper == ItemRendererHelper.ENTITY_ROTATION
+            || type == ItemRenderType.ENTITY && !RenderItem.renderInFrame && helper == ItemRendererHelper.BLOCK_3D;
     }
 
     @Override
     public void renderItem(ItemRenderType type, ItemStack item, Object... data) {
+        IIcon icon = item.getIconIndex();
+        
+        float minU = icon.getMinU();
+        float maxU = icon.getMaxU();
+        float minV = icon.getMinV();
+        float maxV = icon.getMaxV();
+        
         if(type == ItemRenderType.ENTITY) {
-            //RenderBlocks render = (RenderBlocks)data[0];
-            //EntityItem entity = (EntityItem)data[1];
-            
-            IIcon icon = item.getIconIndex();
-            
-            float f = icon.getMinU();
-            float f1 = icon.getMaxU();
-            float f2 = icon.getMinV();
-            float f3 = icon.getMaxV();
-            
-            GL11.glEnable(GL11.GL_BLEND);
-            GL11.glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
-            ItemRenderer.renderItemIn2D(Tessellator.instance, f1, f2, f, f3, icon.getIconWidth(), icon.getIconHeight(), 0.0625f);
+            GL11.glTranslatef(-0.5f, -0.25f, 0.0421875f);
         }
+        
+        GL11.glAlphaFunc(GL11.GL_GREATER, 0.1F);
+        GL11.glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
+        GL11.glEnable(GL11.GL_BLEND);
+        ItemRenderer.renderItemIn2D(Tessellator.instance, maxU, minV, minU, maxV, icon.getIconWidth(), icon.getIconHeight(), 0.0625f);
     }
 }
