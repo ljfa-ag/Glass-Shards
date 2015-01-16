359566a02015ae72ff0779f8326ef9c80a09b0f9
ljfa-ag <ljfa-ag@web.de>
2015-01-16 13:24:26 +0100

    Register models

--- src/main/java/ljfa/glassshards/GlassShards.java
+++ src/main/java/ljfa/glassshards/GlassShards.java
@@ -1,12 +1,22 @@
 package ljfa.glassshards;
 
 import ljfa.glassshards.handlers.HarvestDropsHandler;
+import ljfa.glassshards.items.ItemGlassShards;
 import ljfa.glassshards.items.ModItems;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.ItemMeshDefinition;
+import net.minecraft.client.renderer.ItemModelMesher;
+import net.minecraft.client.renderer.entity.RenderItem;
+import net.minecraft.client.resources.model.ModelBakery;
+import net.minecraft.client.resources.model.ModelResourceLocation;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.MathHelper;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.relauncher.Side;
 
 @Mod(modid = Reference.MODID, name = Reference.MODNAME, version = Reference.VERSION, guiFactory = Reference.GUI_FACTORY_CLASS)
 public class GlassShards {
@@ -29,6 +39,17 @@ public class GlassShards {
         
         /*if(Config.renderTransparent && event.getSide() == Side.CLIENT)
             MinecraftForgeClient.registerItemRenderer(ModItems.glass_shards, new TransparentItemRenderer());*/
+        if(event.getSide() == Side.CLIENT) {
+            ModelBakery.addVariantName(ModItems.glass_shards, ItemGlassShards.variants);
+            
+            ItemModelMesher mesher = Minecraft.getMinecraft().getRenderItem().getItemModelMesher();
+            mesher.register(ModItems.glass_shards, new ItemMeshDefinition() {
+                @Override
+                public ModelResourceLocation getModelLocation(ItemStack stack) {
+                    return new ModelResourceLocation(ItemGlassShards.variants[stack.getItemDamage()], "inventory");
+                }
+            });
+        }
     }
     
     @Mod.EventHandler
