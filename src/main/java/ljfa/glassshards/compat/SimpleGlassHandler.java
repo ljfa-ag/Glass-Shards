0a6feacc5bf8dc83b2f3fefd755d69622726e253
ljfa-ag <ljfa-ag@web.de>
2015-01-18 13:49:43 +0100

    Rename and add comments

---
+++ src/main/java/ljfa/glassshards/compat/SimpleGlassHandler.java
@@ -0,0 +1,36 @@
+package ljfa.glassshards.compat;
+
+import ljfa.glassshards.api.GlassType;
+import net.minecraft.block.Block;
+
+/**
+ * The default implementation of ModGlassHandler.
+ * For stained glass the color is simply inferred from the metadata.
+ * This one can be used most of the time.
+ */
+public class SimpleGlassHandler extends ModGlassHandler {
+    /** Instance for uncolored glass blocks */
+    public static SimpleGlassHandler blockInstance = new SimpleGlassHandler(GlassType.mult_block, false);
+    /** Instance for uncolored glass panes */
+    public static SimpleGlassHandler paneInstance = new SimpleGlassHandler(GlassType.mult_pane, false);
+    /** Instance for stained glass blocks */
+    public static SimpleGlassHandler stainedBlockInstance = new SimpleGlassHandler(GlassType.mult_block, true);
+    /** Instance for stained glass panes */
+    public static SimpleGlassHandler stainedPaneInstance = new SimpleGlassHandler(GlassType.mult_pane, true);
+    
+    protected float multiplier;
+    protected boolean stained;
+    
+    protected SimpleGlassHandler(float multiplier, boolean stained) {
+        this.stained = stained;
+        this.multiplier = multiplier;
+    }
+
+    @Override
+    public GlassType getType(Block block, int meta) {
+        if(stained)
+            return new GlassType(multiplier, true, meta);
+        else
+            return new GlassType(multiplier);
+    }
+}
