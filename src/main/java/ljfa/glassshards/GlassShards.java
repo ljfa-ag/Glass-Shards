63595dcdc85b52f78147596beb9c65bda9ce1f52
ljfa-ag <ljfa-ag@web.de>
2015-01-21 16:32:37 +0100

    Add transparent renderer for sword

--- src/main/java/ljfa/glassshards/GlassShards.java
+++ src/main/java/ljfa/glassshards/GlassShards.java
@@ -36,8 +36,10 @@ public class GlassShards {
         ModRecipes.init();
         MinecraftForge.EVENT_BUS.register(new HarvestDropsHandler());
         
-        if(Config.renderTransparent && event.getSide() == Side.CLIENT)
+        if(Config.renderTransparent && event.getSide() == Side.CLIENT) {
             MinecraftForgeClient.registerItemRenderer(ModItems.glass_shards, new TransparentItemRenderer());
+            MinecraftForgeClient.registerItemRenderer(ModItems.glass_sword, new TransparentItemRenderer());
+        }
     }
     
     @Mod.EventHandler
