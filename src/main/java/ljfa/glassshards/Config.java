236a54fb898e7f998fd08c800c016381ca6f9c4a
ljfa-ag <ljfa-ag@web.de>
2015-01-22 10:51:05 +0100

    Remove unused stuff from config

--- src/main/java/ljfa/glassshards/Config.java
+++ src/main/java/ljfa/glassshards/Config.java
@@ -11,15 +11,11 @@ public class Config {
     public static Configuration conf;
     
     public static final String CATEGORY_GENERAL = "general";
-    //public static final String CATEGORY_CHISEL = "chisel";
     
     public static float shardsChance;
     public static float shardsFortuneChance;
     public static boolean recipesRecolor;
     public static boolean recipeUncolor;
-    
-    /*public static boolean chiselEnable;
-    public static boolean chiselFixPaneDrops;*/
     
     public static void loadConfig(File file) {
         if(conf == null)
@@ -38,16 +34,6 @@ public class Config {
         shardsFortuneChance = (float)conf.get(CATEGORY_GENERAL, "shardsFortuneChance", 0.08, "Chance per fortune level that a block of glass drops shards", 0.0, 1.0).getDouble();
         recipesRecolor = conf.get(CATEGORY_GENERAL, "recipesColor", true, "Add recipes for coloring shards").setRequiresMcRestart(true).getBoolean();
         recipeUncolor = conf.get(CATEGORY_GENERAL, "recipeUncolor", true, "Add recipe to remove the color from shards").setRequiresMcRestart(true).getBoolean();
-        
-        /*conf.getCategory(CATEGORY_CHISEL).setComment("Compatibility options for Chisel 2. Tested with version 2.2.1.");
-        
-        chiselEnable = conf.get(CATEGORY_CHISEL, "activate", true, "Activates compatibility if Chisel is present.\n"
-                + "Note that not activating this even though Chisel is present might lead to unexpected behavior.\n"
-                + "Deactivating this doesn't mean Chisel glass will not drop shards. The shards will just not be stained.").setRequiresMcRestart(true).getBoolean();
-        chiselFixPaneDrops = conf.get(CATEGORY_CHISEL, "fixStainedPanesDrops", true,
-                "By default, Chisel stained glass panes are behaving inconsistently as in they drop themselves\n"
-                + "when broken, unlike all the other Chisel glass types.\n"
-                + "This option changes this behavior and makes them drop shards instead.").getBoolean();*/
         
         if(conf.hasChanged())
             conf.save();
