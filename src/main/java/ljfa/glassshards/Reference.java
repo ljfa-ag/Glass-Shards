d06f94f009577b55557cf0eca0df5cfc8139f784
ljfa-ag <ljfa-ag@web.de>
2015-01-28 18:44:58 +0100

    Change version to 1.0-beta

--- src/main/java/ljfa/glassshards/Reference.java
+++ src/main/java/ljfa/glassshards/Reference.java
@@ -3,7 +3,7 @@ package ljfa.glassshards;
 public class Reference {
     public static final String MODID = "glass_shards";
     public static final String MODNAME = "Glass Shards";
-    public static final String VERSION = "0.4-alpha";
+    public static final String VERSION = "1.0-beta";
     /*public static final String CLIENT_PROXY_CLASS = "ljfa.glassshards.proxy.ClientProxy";
     public static final String SERVER_PROXY_CLASS = "ljfa.glassshards.proxy.CommonProxy";*/
     public static final String GUI_FACTORY_CLASS = "ljfa.glassshards.gui.GsGuiFactory";
