ad831d633ad991595d527d4f53678bef9127d0e2
ljfa-ag <ljfa-ag@web.de>
2015-02-01 10:22:47 +0100

    Nerf glass sword

--- src/main/java/ljfa/glassshards/GlassShards.java
+++ src/main/java/ljfa/glassshards/GlassShards.java
@@ -29,7 +29,7 @@ public class GlassShards {
     @Mod.EventHandler
     public void preInit(FMLPreInitializationEvent event) {
         Config.loadConfig(event.getSuggestedConfigurationFile());
-        toolMatGlass = EnumHelper.addToolMaterial("GLASS", 2, 131, 4.0f, 2.0f, 5);
+        toolMatGlass = EnumHelper.addToolMaterial("GLASS", 2, 109, 4.0f, 2.0f, 5);
         ModItems.preInit();
     }
     
