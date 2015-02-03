123e5244d7add8d88cffac9839bab6435bf6b5c0
ljfa-ag <ljfa-ag@web.de>
2015-02-03 12:13:58 +0100

    Make Config GUI easier to work with

--- src/main/java/ljfa/glassshards/gui/GsConfigGui.java
+++ src/main/java/ljfa/glassshards/gui/GsConfigGui.java
@@ -6,10 +6,10 @@ import java.util.List;
 import ljfa.glassshards.Config;
 import ljfa.glassshards.Reference;
 import net.minecraft.client.gui.GuiScreen;
+import net.minecraftforge.common.config.ConfigCategory;
 import net.minecraftforge.common.config.ConfigElement;
 import cpw.mods.fml.client.config.DummyConfigElement;
 import cpw.mods.fml.client.config.GuiConfig;
-import cpw.mods.fml.client.config.GuiConfigEntries.CategoryEntry;
 import cpw.mods.fml.client.config.IConfigElement;
 
 public class GsConfigGui extends GuiConfig {
@@ -17,25 +17,15 @@ public class GsConfigGui extends GuiConfig {
         super(parent, getConfigElements(), Reference.MODID, false, false, "Glass Shards configuration");
     }
     
-    /** Compiles a list of config elements */
+    /** Compiles a list of config elements
+     * Borrowed from EnderIO's implementation
+     */
     private static List<IConfigElement> getConfigElements() {
         List<IConfigElement> list = new ArrayList<IConfigElement>();
         
-        //Add categories to config GUI
-        list.add(categoryElement(Config.CATEGORY_GENERAL, "General", "glass_shards.configgui.ctgy.general"));
-        list.add(categoryElement(Config.CATEGORY_CHISEL, "Chisel compatibility", "glass_shards.configgui.ctgy.chisel"));
-        list.add(categoryElement(Config.CATEGORY_TCONSTRUCT, "Tinkers Construct compatibility", "glass_shards.configgui.ctgy.tconstruct"));
-        list.add(categoryElement(Config.CATEGORY_MFR, "MineFactory Reloaded compatibility", "glass_shards.configgui.ctgy.mfr"));
-        list.add(categoryElement(Config.CATEGORY_ENDERIO, "EnderIO compatibility", "glass_shards.configgui.ctgy.enderio"));
-        list.add(categoryElement(Config.CATEGORY_THERMALEXP, "Thermal Expansion compatibility", "glass_shards.configgui.ctgy.thermalexp"));
-        list.add(categoryElement(Config.CATEGORY_THAUMCRAFT, "Thaumcraft compatibility", "glass_shards.configgui.ctgy.thaumcraft"));
+        for(String name: Config.conf.getCategoryNames())
+            list.add(new ConfigElement<ConfigCategory>(Config.conf.getCategory(name)));
         
         return list;
-    }
-    
-    /** Creates a button linking to another screen where all options of the category are available */
-    private static IConfigElement categoryElement(String category, String name, String tooltip_key) {
-        return new DummyConfigElement.DummyCategoryElement(name, tooltip_key,
-                new ConfigElement(Config.conf.getCategory(category)).getChildElements());
     }
 }
