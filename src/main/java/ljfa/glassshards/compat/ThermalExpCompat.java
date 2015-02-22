dc0e37e0632a9b64f18ce448dcc737feebe468af
ljfa-ag <ljfa-ag@web.de>
2015-02-22 12:13:13 +0100

    Use ThermalExpansionHelper instead

--- src/main/java/ljfa/glassshards/compat/ThermalExpCompat.java
+++ src/main/java/ljfa/glassshards/compat/ThermalExpCompat.java
@@ -4,13 +4,13 @@ import ljfa.glassshards.items.ModItems;
 import ljfa.glassshards.util.LogHelper;
 import net.minecraft.init.Blocks;
 import net.minecraft.item.ItemStack;
-import thermalexpansion.util.crafting.PulverizerManager;
+import cofh.api.modhelpers.ThermalExpansionHelper;
 
 public class ThermalExpCompat {
     public static void addRecipes() {
-        PulverizerManager.removeRecipe(new ItemStack(Blocks.glass));
-        PulverizerManager.addRecipe(3200, new ItemStack(Blocks.glass), new ItemStack(ModItems.glass_shards, 1, 16));
-        PulverizerManager.addRecipe(1600, new ItemStack(ModItems.glass_shards, 1, 16), new ItemStack(Blocks.sand));
+        //PulverizerManager.removeRecipe(new ItemStack(Blocks.glass));
+        ThermalExpansionHelper.addPulverizerRecipe(3200, new ItemStack(Blocks.glass), new ItemStack(ModItems.glass_shards, 1, 16));
+        ThermalExpansionHelper.addPulverizerRecipe(1600, new ItemStack(ModItems.glass_shards, 1, 16), new ItemStack(Blocks.sand));
         LogHelper.info("Successfully added Thermal Expansion Pulverizer recipes.");
     }
 }
