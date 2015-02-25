7018e9b53d0aa6f0935c693934c361a91725bf9d
ljfa-ag <ljfa-ag@web.de>
2015-02-25 13:21:22 +0100

    Use Gradle to fill in the version number

--- src/main/java/ljfa/glassshards/Reference.java
+++ src/main/java/ljfa/glassshards/Reference.java
@@ -3,7 +3,7 @@ package ljfa.glassshards;
 public class Reference {
     public static final String MODID = "glass_shards";
     public static final String MODNAME = "Glass Shards";
-    public static final String VERSION = "1.1.1";
+    public static final String VERSION = "@VERSION@";
     /*public static final String CLIENT_PROXY_CLASS = "ljfa.glassshards.proxy.ClientProxy";
     public static final String SERVER_PROXY_CLASS = "ljfa.glassshards.proxy.CommonProxy";*/
     public static final String GUI_FACTORY_CLASS = "ljfa.glassshards.gui.GsGuiFactory";
