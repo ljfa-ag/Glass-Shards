99be0fd7e1fff814c119d91f55c1362f8ca142ce
ljfa-ag <ljfa-ag@web.de>
2015-02-26 17:07:06 +0100

    Rework HarvestDropsHandler

--- src/main/java/ljfa/glassshards/handlers/HarvestDropsHandler.java
+++ src/main/java/ljfa/glassshards/handlers/HarvestDropsHandler.java
@@ -2,10 +2,9 @@ package ljfa.glassshards.handlers;
 
 import ljfa.glassshards.Config;
 import ljfa.glassshards.api.GlassType;
+import ljfa.glassshards.api.IShatterableGlass;
 import ljfa.glassshards.items.ModItems;
-import ljfa.glassshards.util.GlassHelper;
 import ljfa.glassshards.util.GlassRegistry;
-import ljfa.glassshards.util.LogHelper;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.event.world.BlockEvent;
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
@@ -17,20 +16,24 @@ public class HarvestDropsHandler {
         if(event.world.isRemote || event.isSilkTouching)
             return;
         
-        // Sometimes the list of drops is not empty even if the drop chance is set to zero.
-        // This might lead to duplication bugs.
-        if(!event.drops.isEmpty()) {
-            if(event.dropChance < 1e-6f) {
-                event.drops.clear();
-                LogHelper.info("%s has a negligible drop chance, but the drop list is not empty", event.block.getUnlocalizedName());
-            }
-            else if(GlassRegistry.shouldRemoveDrop(event.block, event.blockMetadata))
-                event.drops.clear();
-            else
-                return;
+        if(event.block instanceof IShatterableGlass) {
+            addShardsDrop(event, ((IShatterableGlass)event.block).getType(event.block, event.blockMetadata));
         }
+        else { 
+            // Sometimes the list of drops is not empty even if the drop chance is set to zero.
+            // This might lead to duplication bugs.
+            if(!event.drops.isEmpty()) {
+                if(event.dropChance < 1e-6f || GlassRegistry.shouldRemoveDrop(event.block, event.blockMetadata))
+                    event.drops.clear();
+                else
+                    return;
+            }
         
-        GlassType gtype = GlassHelper.getType(event.block, event.blockMetadata);
+            addShardsDrop(event, GlassRegistry.getType(event.block, event.blockMetadata));
+        }
+    }
+    
+    private void addShardsDrop(BlockEvent.HarvestDropsEvent event, GlassType gtype) {
         if(gtype != null) {
             int meta = gtype.isStained() ? gtype.getColor() : 16;
             event.drops.add(new ItemStack(ModItems.glass_shards, 1, meta));
