fcca35dc040e9a04a440c06b0fc0e065ae656f29
ljfa-ag <ljfa-ag@web.de>
2015-02-28 19:24:03 +0100

    Change EnderIO compat stuff
    Make Enlightened Clear Glass drop shards and glowstone
    Add recipe for Enlightened Clear Glass using shards

--- src/main/java/ljfa/glassshards/compat/EnderIOGlassHelper.java
+++ src/main/java/ljfa/glassshards/compat/EnderIOGlassHelper.java
@@ -11,7 +11,9 @@ import ljfa.glassshards.util.LogHelper;
 import ljfa.glassshards.util.ModGlassHandler;
 import ljfa.glassshards.util.ReflectionHelper;
 import net.minecraft.block.Block;
+import net.minecraft.init.Items;
 import net.minecraft.item.ItemStack;
+import net.minecraftforge.event.world.BlockEvent.HarvestDropsEvent;
 import net.minecraftforge.oredict.OreDictionary;
 
 import org.apache.logging.log4j.Level;
@@ -23,13 +25,21 @@ import crazypants.enderio.machine.recipe.OreDictionaryRecipeInput;
 import crazypants.enderio.machine.recipe.RecipeInput;
 
 public class EnderIOGlassHelper {
+    public static final int clear_meta = 1, enlightened_meta = 3;
+    
     public static void init() {
-        //Add block to the registry
-        //meta 1 = quite clear glass
+        
         GlassRegistry.addHandler(EnderIO.blockFusedQuartz, new ModGlassHandler() {
             @Override
+            public void addShardsDrop(HarvestDropsEvent event) {
+                super.addShardsDrop(event);
+                if(event.blockMetadata == enlightened_meta)
+                    event.drops.add(new ItemStack(Items.glowstone_dust, 4));
+            }
+            
+            @Override
             public GlassType getType(Block block, int meta) {
-                if(meta == 1)
+                if(meta == clear_meta || meta == enlightened_meta)
                     return new GlassType(GlassType.mult_block);
                 else
                     return null;
@@ -37,7 +47,7 @@ public class EnderIOGlassHelper {
             
             @Override
             public boolean shouldRemoveDrop(Block block, int meta) {
-                return meta == 1;
+                return meta == clear_meta || meta == enlightened_meta;
             }
         });
         
@@ -100,6 +110,7 @@ public class EnderIOGlassHelper {
         
         if(Config.eioAlloySmelter) {
             //Alloy Smelter: glass shards -> quite clear glass
+            //and glass shards + 4 glowstone dust -> enlightened clear glass
             FMLInterModComms.sendMessage("EnderIO", "recipe:alloysmelter",
                 "<recipeGroup name=\"GlassShards\">" +
                   "<recipe name=\"Fused Glass\" energyCost=\"2500\">" +
@@ -110,6 +121,15 @@ public class EnderIOGlassHelper {
                       "<itemStack modID=\"EnderIO\" itemName=\"blockFusedQuartz\" itemMeta=\"1\" exp=\"0.2\" />" +
                     "</output>" +
                   "</recipe>" +
+                  "<recipe name=\"Enlightened Fused Glass\" energyCost=\"2500\">" +
+                  "<input>" +
+                    "<itemStack modID=\"glass_shards\" itemName=\"glass_shards\" itemMeta=\"16\" />" +
+                    "<itemStack modID=\"minecraft\" itemName=\"glowstone_dust\" number=\"4\" />" +
+                  "</input>" +
+                  "<output>" +
+                    "<itemStack modID=\"EnderIO\" itemName=\"blockFusedQuartz\" itemMeta=\"3\" exp=\"0.2\" />" +
+                  "</output>" +
+                "</recipe>" +
                 "</recipeGroup>");
         }
     }
@@ -122,10 +142,10 @@ public class EnderIOGlassHelper {
         try {
             CrusherRecipeManager recipeManager = CrusherRecipeManager.getInstance();
             List<RecipeInput> excludes = (List<RecipeInput>)ReflectionHelper.getField(CrusherRecipeManager.class, "ballExcludes", recipeManager);
-            excludes.add(new OreDictionaryRecipeInput(new ItemStack(ModItems.glass_shards, 16), OreDictionary.getOreID("shardsGlass"), 0));
+            excludes.add(new OreDictionaryRecipeInput(new ItemStack(ModItems.glass_shards, 1, 16), OreDictionary.getOreID("shardsGlass"), 0));
             
             LogHelper.info("Successfully added to EnderIO grinding ball excludes list.");
-        } catch(Exception ex) {
+        } catch(ReflectiveOperationException ex) {
             LogHelper.log(Level.ERROR, ex, "Failed to manipulate EnderIO grinding ball excludes list.");
             return;
         }
