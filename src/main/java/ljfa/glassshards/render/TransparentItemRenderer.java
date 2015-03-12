422535c2f0b4b55f85981faa9463f6dc035af457
ljfa-ag <ljfa-ag@web.de>
2015-03-12 10:29:31 +0100

    Add enchantment glow

--- src/main/java/ljfa/glassshards/render/TransparentItemRenderer.java
+++ src/main/java/ljfa/glassshards/render/TransparentItemRenderer.java
@@ -2,14 +2,21 @@ package ljfa.glassshards.render;
 
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.ItemRenderer;
+import net.minecraft.client.renderer.OpenGlHelper;
 import net.minecraft.client.renderer.Tessellator;
 import net.minecraft.client.renderer.entity.RenderItem;
+import net.minecraft.client.renderer.texture.TextureManager;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.IIcon;
+import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.client.IItemRenderer;
 
 import org.lwjgl.opengl.GL11;
 
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
+
+@SideOnly(Side.CLIENT)
 public class TransparentItemRenderer implements IItemRenderer {
     @Override
     public boolean handleRenderType(ItemStack item, ItemRenderType type) {
@@ -47,6 +54,37 @@ public class TransparentItemRenderer implements IItemRenderer {
         GL11.glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
         GL11.glEnable(GL11.GL_BLEND);
         ItemRenderer.renderItemIn2D(Tessellator.instance, maxU, minV, minU, maxV, icon.getIconWidth(), icon.getIconHeight(), 0.0625f);
+        if(item.hasEffect(0)) {
+            //Borrowed and adapted from Mojang code
+            GL11.glDepthFunc(GL11.GL_EQUAL);
+            GL11.glDisable(GL11.GL_LIGHTING);
+            textureManager.bindTexture(resourceEnchGlint);
+            OpenGlHelper.glBlendFunc(768, 1, 1, 0);
+            float f7 = 0.76F;
+            GL11.glColor4f(0.5F * f7, 0.25F * f7, 0.8F * f7, 1.0F);
+            GL11.glMatrixMode(GL11.GL_TEXTURE);
+            GL11.glPushMatrix();
+            float f8 = 0.125F;
+            GL11.glScalef(f8, f8, f8);
+            float f9 = (float)(Minecraft.getSystemTime() % 3000L) / 3000.0F * 8.0F;
+            GL11.glTranslatef(f9, 0.0F, 0.0F);
+            GL11.glRotatef(-50.0F, 0.0F, 0.0F, 1.0F);
+            ItemRenderer.renderItemIn2D(Tessellator.instance, 0.0F, 0.0F, 1.0F, 1.0F, 256, 256, 0.0625F);
+            GL11.glPopMatrix();
+            GL11.glPushMatrix();
+            GL11.glScalef(f8, f8, f8);
+            f9 = (float)(Minecraft.getSystemTime() % 4873L) / 4873.0F * 8.0F;
+            GL11.glTranslatef(-f9, 0.0F, 0.0F);
+            GL11.glRotatef(10.0F, 0.0F, 0.0F, 1.0F);
+            ItemRenderer.renderItemIn2D(Tessellator.instance, 0.0F, 0.0F, 1.0F, 1.0F, 256, 256, 0.0625F);
+            GL11.glPopMatrix();
+            GL11.glMatrixMode(GL11.GL_MODELVIEW);
+            GL11.glEnable(GL11.GL_LIGHTING);
+            GL11.glDepthFunc(GL11.GL_LEQUAL);
+        }
         GL11.glDisable(GL11.GL_BLEND);
     }
+    
+    private static final TextureManager textureManager = Minecraft.getMinecraft().getTextureManager();
+    private static final ResourceLocation resourceEnchGlint = new ResourceLocation("textures/misc/enchanted_item_glint.png");
 }
