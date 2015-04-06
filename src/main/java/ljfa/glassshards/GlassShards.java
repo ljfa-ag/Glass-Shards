4443358d476a2df429b58929f6b7e79cdc5cdde8
ljfa-ag <ljfa-ag@web.de>
2015-04-06 13:33:05 +0200

    Use ComparableVersion to compare Chisel versions

--- src/main/java/ljfa/glassshards/GlassShards.java
+++ src/main/java/ljfa/glassshards/GlassShards.java
@@ -22,6 +22,7 @@ import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import cpw.mods.fml.common.versioning.ComparableVersion;
 import cpw.mods.fml.relauncher.Side;
 
 @Mod(modid = Reference.MODID, name = Reference.MODNAME, version = Reference.VERSION, guiFactory = Reference.GUI_FACTORY_CLASS)
@@ -31,17 +32,34 @@ public class GlassShards {
 
     public static ToolMaterial toolMatGlass;
 
+    public boolean isChiselCorrectVersion;
+    
     @Mod.EventHandler
     public void preInit(FMLPreInitializationEvent event) {
         Config.loadConfig(event.getSuggestedConfigurationFile());
         toolMatGlass = EnumHelper.addToolMaterial("GLASS", 2, Config.swordDurability, 4.0f, 2.0f, 5);
         ModItems.preInit();
         toolMatGlass.setRepairItem(new ItemStack(ModItems.glass_shards, 1, 16));
+        
+        if(Loader.isModLoaded("chisel")) {
+            if(new ComparableVersion(Loader.instance().getIndexedModList().get("chisel").getVersion())
+                    .compareTo(new ComparableVersion("2.3")) >= 0) {
+                isChiselCorrectVersion = true;
+            }
+            else {
+                LogHelper.warn("The Chisel compatibility requires at least version 2.3");
+                isChiselCorrectVersion = false;
+            }
+        }
+        else
+            isChiselCorrectVersion = false;
+        
     }
 
     @Mod.EventHandler
     public void init(FMLInitializationEvent event) {
         ModRecipes.init();
+        
         MinecraftForge.EVENT_BUS.register(new HarvestDropsHandler());
         if(Config.incrBreakSpeed)
             MinecraftForge.EVENT_BUS.register(new BreakSpeedHandler());
@@ -64,13 +82,9 @@ public class GlassShards {
     }
 
     /** Initialize compatibility with other mods */
-    private static void initCompatModules() {
-        if(Config.chiselEnable && Loader.isModLoaded("chisel")) {
-            if(Loader.instance().getIndexedModList().get("chisel").getVersion().startsWith("2.3"))
-                ChiselGlassHelper.init();
-            else
-                LogHelper.warn("The Chisel compatibility requires version 2.3.x");
-        }
+    private void initCompatModules() {
+        if(Config.chiselEnable && isChiselCorrectVersion)
+            ChiselGlassHelper.init();
         if(Loader.isModLoaded("TConstruct")) {
             if(Config.tinkersEnable)
                 TinkersGlassHelper.init();
