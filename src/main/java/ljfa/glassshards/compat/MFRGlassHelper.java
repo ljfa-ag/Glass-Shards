44cc91c028ef881b112b8c77c3bc1ae0ae8f5786
ljfa-ag <ljfa-ag@web.de>
2015-09-06 21:02:06 +0200

    Change MFR and TiCo compatibility to use block names

--- src/main/java/ljfa/glassshards/compat/MFRGlassHelper.java
+++ src/main/java/ljfa/glassshards/compat/MFRGlassHelper.java
@@ -4,17 +4,11 @@ import static ljfa.glassshards.GlassShards.logger;
 
 import ljfa.glassshards.glass.GlassRegistry;
 import ljfa.glassshards.glass.SimpleGlassHandler;
-import net.minecraft.block.Block;
-import powercrystals.minefactoryreloaded.setup.MFRThings;
 
 public class MFRGlassHelper {
     public static void init() {
-        Block mfrStainedGlass = MFRThings.factoryGlassBlock;
-        Block mfrStainedPane = MFRThings.factoryGlassPaneBlock;
-        
-        //Add blocks to the registry
-        GlassRegistry.addHandler(mfrStainedGlass, SimpleGlassHandler.stainedBlockInstance);
-        GlassRegistry.addHandler(mfrStainedPane, SimpleGlassHandler.stainedPaneInstance);
+        GlassRegistry.addHandler("MineFactoryReloaded:stainedglass.block", SimpleGlassHandler.stainedBlockInstance);
+        GlassRegistry.addHandler("MineFactoryReloaded:stainedglass.pane", SimpleGlassHandler.stainedPaneInstance);
         
         logger.info("Successfully loaded MineFactory Reloaded compatibility.");
     }
--- src/main/java/ljfa/glassshards/compat/TinkersGlassHelper.java
+++ src/main/java/ljfa/glassshards/compat/TinkersGlassHelper.java
@@ -5,31 +5,26 @@ import static ljfa.glassshards.GlassShards.logger;
 import ljfa.glassshards.glass.GlassRegistry;
 import ljfa.glassshards.glass.SimpleGlassHandler;
 import ljfa.glassshards.items.ModItems;
-import net.minecraft.block.Block;
 import net.minecraft.init.Blocks;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.fluids.Fluid;
+import net.minecraftforge.fluids.FluidRegistry;
 import net.minecraftforge.fluids.FluidStack;
 import tconstruct.library.crafting.Smeltery;
-import tconstruct.smeltery.TinkerSmeltery;
 
 public class TinkersGlassHelper {
     public static void init() {
-        Block tcGlass = TinkerSmeltery.clearGlass;
-        Block tcPane = TinkerSmeltery.glassPane;
-        Block tcStainedGlass = TinkerSmeltery.stainedGlassClear;
-        
         //Add blocks to the registry
-        GlassRegistry.addHandler(tcGlass, SimpleGlassHandler.clearingBlockInstance);
-        GlassRegistry.addHandler(tcPane, SimpleGlassHandler.clearingPaneInstance);
-        GlassRegistry.addHandler(tcStainedGlass, SimpleGlassHandler.clearingStainedBlockInstance);
+        GlassRegistry.addHandler("TConstruct:GlassBlock", SimpleGlassHandler.clearingBlockInstance);
+        GlassRegistry.addHandler("TConstruct:GlassPane", SimpleGlassHandler.clearingPaneInstance);
+        GlassRegistry.addHandler("TConstruct:GlassBlock.StainedClear", SimpleGlassHandler.clearingStainedBlockInstance);
         //Stained panes work fine without need to register them here
         
         logger.info("Successfully loaded Tinkers Construct compatibility.");
     }
     
     public static void addSmelteryRecipe() {
-        Fluid moltenGlass = TinkerSmeltery.moltenGlassFluid;
+        Fluid moltenGlass = FluidRegistry.getFluid("glass.molten");
         Smeltery.addMelting(new ItemStack(ModItems.glass_shards, 1, 16),
                 Blocks.glass, 0, 625, new FluidStack(moltenGlass, 1000));
     }
