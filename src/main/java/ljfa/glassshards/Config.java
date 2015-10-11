d59abb4cb4b5570c90e12d1344a6891d6aad7bff
ljfa-ag <ljfa-ag@web.de>
2015-10-11 17:45:39 +0200

    Add config option for disabling furnace recipes
    Conflicts:
    	src/main/java/ljfa/glassshards/Config.java

--- src/main/java/ljfa/glassshards/Config.java
+++ src/main/java/ljfa/glassshards/Config.java
@@ -16,6 +16,7 @@ public class Config {
     public static float shardsFortuneChance;
     public static boolean recipesRecolor;
     public static boolean recipeUncolor;
+    public static boolean recipesFurnace;
     public static boolean enableSword;
     public static int swordDurability;
     public static boolean incrBreakSpeed;
@@ -37,6 +38,7 @@ public class Config {
         shardsFortuneChance = (float)conf.get(CAT_GENERAL, "shardsFortuneChance", 0.07, "Chance per fortune level that a block of glass drops shards", 0.0, 1.0).getDouble();
         recipesRecolor = conf.get(CAT_GENERAL, "recipesColor", true, "Add recipes for coloring shards").setRequiresMcRestart(true).getBoolean();
         recipeUncolor = conf.get(CAT_GENERAL, "recipeUncolor", true, "Add recipe to remove the color from shards").setRequiresMcRestart(true).getBoolean();
+        recipesFurnace = conf.get(CAT_GENERAL, "recipesFurnace", true, "Add furnace recipes to smelt shards to glass blocks\n(if you disable this, you will probably want to add some other way to process shards)").setRequiresMcRestart(true).getBoolean();
         enableSword = conf.get(CAT_GENERAL, "enableSword", true, "Enables the glass sword").setRequiresMcRestart(true).getBoolean();
         swordDurability = conf.get(CAT_GENERAL, "swordDurability", 109, "Durability of the glass sword", 1, 1561).setRequiresMcRestart(true).getInt();
         incrBreakSpeed = conf.get(CAT_GENERAL, "increaseGlassBreakSpeed", true, "Glass breaks faster when mined with a pickaxe").setRequiresMcRestart(true).getBoolean();
